7cc5e4d71f8827c2078c51cb3c53c555867893c2
Riley Lozon <lozonr@uwindsor.ca>
2025-05-23 00:18:52 -0400

    Bot is running no way

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,37 @@
-fn main() {
-    printer();
+use poise::serenity_prelude as serenity;
+use std::env;
+
+struct Data {}
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+#[poise::command(slash_command, prefix_command)]
+async fn ping(ctx: Context<'_>) -> Result<(), Error> {
+    let response = "Pong!";
+    ctx.say(response).await?;
+    Ok(())
 }
 
-fn printer(){
-    println!("WHAT THE HECK");
+#[tokio::main]
+async fn main() {
+    let token = env::var("TOKEN").expect("Expected a token in the environment");
+    let intents = serenity::GatewayIntents::non_privileged();
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![ping()],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+    client.unwrap().start().await.unwrap();
 }
